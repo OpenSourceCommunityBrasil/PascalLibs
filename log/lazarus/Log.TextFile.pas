c3487f58a10051a6cd86e195e00a63cc8dc1e37e
Fernando Castelano Banhos <ferncb@yahoo.com.br>
2024-07-20 21:07:33 -0300

    - classe remodelada afim, para evitar concorrencia de gravação mesmo arquivo

--- log/lazarus/Log.TextFile.pas
+++ log/lazarus/Log.TextFile.pas
@@ -8,82 +8,174 @@ unit Log.TextFile;
 interface
 
 uses
-  SysUtils, Classes;
+  SysUtils, Classes, syncobjs;
 
 type
+
+  { TInfoLog }
+
+  TInfoLog = class
+  private
+    FFileName: string;
+    FSection: string;
+    FValue: string;
+  public
+    function WriteLog : boolean;
+  published
+    property FileName: string read FFileName write FFileName;
+    property Section: string  read FSection write FSection;
+    property Value: string  read FValue write FValue;
+  end;
 
   { TLog }
 
   TLog = class(TThread)
   private
-    FFileName: string;
-    FSection: string;
-    FValue: string;
+    FInfo : TList;
+    FCritical : TCriticalSection;
+    FEvent : TSimpleEvent;
   protected
-    procedure doLog;
     procedure Execute; override;
+    procedure Clear;
   public
-    constructor Create(ASection, AValue, AFileName: string);
-    class procedure Log(ASection, AValue: string; AFileName: string = 'log.txt');
+    constructor Create;
     destructor Destroy; override;
+
+    procedure Terminar;
+
+    procedure AddLog(AInfo : TInfoLog);
+
+    class procedure Log(ASection, AValue: string; AFileName: string = 'log.txt');
   end;
 
 implementation
 
+var
+  vLogger : TLog;
+
+{ TInfoLog }
+
+function TInfoLog.WriteLog: boolean;
+var
+  vLogFile: Text;
+  vLogData: String;
+begin
+  vLogData := FormatDateTime('dd/mm/yyyy hh:nn:ss', Now) + ' | ' +
+              FSection + ' | ' + FValue;
+
+  Result := False;
+  try
+    AssignFile(vLogFile, FFileName);
+    try
+      if FileExists(FFileName) then
+        Append(vLogFile)
+      else
+        Rewrite(vLogFile);
+
+      Writeln(vLogFile, vLogData);
+      Result := True;
+    finally
+      CloseFile(vLogFile);
+    end;
+  except
+    Result := False;
+  end;
+end;
+
 { TLog }
 
-constructor TLog.Create(ASection, AValue, AFileName: string);
+constructor TLog.Create;
 begin
-  inherited Create(False);
   FreeOnTerminate := True;
-  FFileName := AFileName;
-  FSection := ASection;
-  FValue := AValue;
+  FInfo := TList.Create;
+  FCritical := TCriticalSection.Create;
+  FEvent := TSimpleEvent.Create;
+
+  inherited Create(False);
 end;
 
 destructor TLog.Destroy;
 begin
-
+  Clear;
+  FreeAndNil(FInfo);
+  FreeAndNil(FCritical);
+  FreeAndNil(FEvent);
   inherited Destroy;
 end;
 
-procedure TLog.doLog;
-var
-  FLogFile: Text;
-  logdata: string;
-  Confirma: boolean;
+procedure TLog.Terminar;
 begin
-  logdata := FormatDateTime('dd/mm/yyyy hh:nn:ss', Now) + ' | ' +
-    FSection + ' | ' + FValue;
+  Terminate;
+  FEvent.SetEvent;
+end;
 
-  Confirma := False;
-  while not Confirma do
-  try
-    try
-      AssignFile(FLogFile, FFileName);
-      if FileExists(FFileName) then
-        Append(FLogFile)
-      else
-        Rewrite(FLogFile);
+procedure TLog.AddLog(AInfo: TInfoLog);
+begin
+  FCritical.Enter;
+  FInfo.Add(AInfo);
+  FCritical.Leave;
 
-      Writeln(FlogFile, logdata);
-      Confirma := True;
-    except
-      Confirma := False;
-    end;
-  finally
-    CloseFile(FlogFile);
-  end;
+  FEvent.SetEvent;
 end;
 
 procedure TLog.Execute;
+var
+  vInfo : TInfoLog;
 begin
-  Synchronize(@doLog);
+  while not Terminated do
+  begin
+    FEvent.WaitFor(INFINITE);
+
+    if FInfo.Count > 0 then
+    begin
+      FCritical.Enter;
+      vInfo := TInfoLog(FInfo.Items[0]);
+      if vInfo.WriteLog then
+      begin
+        FreeAndNil(vInfo);
+        FInfo.Delete(0);
+      end;
+      FCritical.Leave;
+    end
+    else
+    begin
+      FEvent.ResetEvent;
+    end;
+  end;
+end;
+
+procedure TLog.Clear;
+var
+  vInfo : TInfoLog;
+begin
+  while FInfo.Count > 0 do
+  begin
+    vInfo := TInfoLog(FInfo.Items[FInfo.Count - 1]);
+    FreeAndNil(vInfo);
+    FInfo.Delete(FInfo.Count - 1);
+  end;
 end;
 
 class procedure TLog.Log(ASection, AValue: string; AFileName: string);
+var
+  vInfo : TInfoLog;
 begin
-  Create(ASection, AValue, AFileName);
+  if vLogger = nil then
+    vLogger := TLog.Create;
+
+  vInfo := TInfoLog.Create;
+  vInfo.Section := ASection;
+  vInfo.Value := AValue;
+  vInfo.FileName := AFileName;
+
+  vLogger.AddLog(vInfo);
 end;
+
+initialization
+  vLogger := nil;
+
+finalization
+  if vLogger <> nil then
+    vLogger.Terminar;
 
 end.
